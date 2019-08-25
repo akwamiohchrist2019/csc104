3930e58c30659d67a4b846deb9aad6a0c6a49cf2
aweni2019 <ibrahinsafiya88@gmail.com>
2019-08-25 22:50:05 +0100

    commit

---
+++ code.py
@@ -0,0 +1,21 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> import math
+>>> x = int(input("Enter X Number:"))
+Enter X Number:56
+>>> y = int(input("Enter Y Number:"))
+Enter Y Number:6
+>>> answer = x**y
+>>> print (answer)
+30840979456
+>>> print ("x raised to the power of y is:", answer)
+x raised to the power of y is: 30840979456
+>>> print (math.log(x))
+4.02535169073515
+>>> print (math.log(x, 2))
+5.807354922057605
+>>> print (math.log2(x))
+5.807354922057604
+>>> print (x^y)
+62
+>>> 
